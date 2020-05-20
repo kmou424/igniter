3f990f227ad65633b63e3b0acc9487104ed45b08
kmou424 <dx2689282850@gmail.com>
2020-05-20 09:42:53 +0800

    Open url when About and Credits items are clicked

--- app/src/main/java/io/github/trojan_gfw/igniter/AboutActivity.java
+++ app/src/main/java/io/github/trojan_gfw/igniter/AboutActivity.java
@@ -2,10 +2,13 @@ package io.github.trojan_gfw.igniter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -27,6 +30,25 @@ public class AboutActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+	    getPreferenceScreen().setOnPreferenceClickListener(new PreferenceScreen.OnPreferenceClickListener() {
+		    public boolean onPreferenceClick(Preference preference) {
+			boolean isHandled = true;
+			String preference_summary = preference.getSummary().toString();
+			if(preference_summary.length() > 4){
+			    switch(preference_summary.toString().substring(0, 3)) {
+				case "http":
+				    Uri uri = Uri.parse(preference_summary);
+				    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+				    startActivity(intent);
+				    break;
+			    }
+			    isHandled = true;
+			} else {
+			    isHandled = false;
+			}
+			return isHandled;
+		    }
+		});
         }
     }
 }
